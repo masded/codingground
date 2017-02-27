799b5ce9e9e61e786b396d58a25f68c081b43305
masded <dedydwiarfiyanto@gmail.com>
2017-02-27 09:35:31 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas3.java
@@ -0,0 +1,14 @@
+public class tugas3{
+    
+    public static void main(String []args){
+        int x = 3;
+        int y = Angka(x);
+        System.out.println("angka yang anda masukkan adalah angka :" + y);
+    }
+    
+    public static int Angka(int x1){
+        int angka = x1;
+        
+        return angka;
+        }
+    }
