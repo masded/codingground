392901f9da914eb1784c21e2da37c26a36c8240f
masded <dedydwiarfiyanto@gmail.com>
2017-02-27 09:55:06 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/tugas2.java
@@ -0,0 +1,11 @@
+public class tugas2{
+    
+    public static void main(String []args){
+        String[] nama = new String []{
+            "DEDY", "DWI", "ARFIYANTO"
+        };
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+    }
+}
