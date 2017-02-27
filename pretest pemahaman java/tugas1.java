7e1b77a99c67762912d4a605ad32ff46986bd5c2
masded <dedydwiarfiyanto@gmail.com>
2017-02-27 09:53:19 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+    
+    public static void main(String []args){
+        String nama = "dedy dwi arfiyanto";
+        String alamat = "kecapi Jepara";
+        System.out.println("nama : " + nama + "\n" + "alamat : " + alamat);
+    }
+}
